325d671779ef0b80cb7ed58b0a76252cc46ae709
Colin Chen <colin.a.chen@gmail.com>
2018-10-04 10:18:47 -0700

    Added example pictures with all letters in word mocha

---
+++ processImage.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+
+'''
+Colin Chen October 2018
+
+Take a screenshot from wordMocha (eventually run in app?)
+
+Template match each letter
+Find all possible words with those letters
+
+'''
+
+
+#letters from black to white
+def formatLetters(path):
