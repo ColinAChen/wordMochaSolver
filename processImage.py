38141fca5286776672171b8a4ba53c9262642db4
Colin Chen <colin.a.chen@gmail.com>
2018-11-10 13:33:58 -0800

    comparing ratio of letter to screenshot and record location

--- processImage.py
+++ processImage.py
@@ -3,8 +3,10 @@ import numpy as np
 import imutils
 import cv2
 import sys
+import math
 
 import wordMocha
+#ML TO DETERMINE LIKLINESS THAT WORD IS ENGLISH
 '''
 Colin Chen October 2018
 
@@ -32,6 +34,8 @@ def display (title, img):
 #display ('pathTest',testImage )
 #letters from black to white
 #format a single image (go to black and white)
+def distance(point1, point2):
+	return math.sqrt(((point1[0] - point2[0]) ** 2) + ((point1[1] - point2[1]) ** 2) )
 def formatLetter(img): 
 	
 	
@@ -90,12 +94,18 @@ def formatScreenshot(imagePath,screenshotPos):
 
 def matchLetters(gameScreenshotPath,screenshotPos, alphabetPath):
 	final = {}
+
 	locations = []
+	totalRatio=0
+	count=0
+	letterToScren = 0
+	iteration = 0
 	screenshot = cv2.imread(gameScreenshotPath + '/' + os.listdir(gameScreenshotPath)[screenshotPos], cv2.IMREAD_GRAYSCALE)
 	for scale in np.linspace(0.2,2,100)[::-1]:
 		print ('SCALE:',scale)
 		resized = imutils.resize(screenshot, width = int(screenshot.shape[1] * scale))
 		r = screenshot.shape[1] / float(resized.shape[1]) #ratio of old to new
+
 		for letterName in os.listdir(alphabetPath):
 
 			#print('Looking for', str(letterName)[0:1])
@@ -104,6 +114,7 @@ def matchLetters(gameScreenshotPath,screenshotPos, alphabetPath):
 			letterImage = cv2.imread(alphabetPath + '/' + str(letterName), cv2.IMREAD_GRAYSCALE)
 			rows,cols = letterImage.shape
 			tempRows,tempCols = resized.shape
+
 			if (tempRows > rows and tempCols > cols):
 				res = cv2.matchTemplate(resized, letterImage, cv2.TM_CCOEFF_NORMED)
 				(_,maxVal,_,maxLoc) = cv2.minMaxLoc(res)
@@ -112,18 +123,74 @@ def matchLetters(gameScreenshotPath,screenshotPos, alphabetPath):
 				#print (maxVal)
 				threshold = 0.9
 				if (maxVal >= threshold): #only if decent match
+					letterToScreen = cols/(resized.shape[1])
+					totalRatio += letterToScreen
+					count += 1
 					print (str(letterName[0:1]), maxVal)
+					print ('RATIO', letterToScreen)
+					print ('MAXLOC', maxLoc)
 					if (str(letterName[0:1]) not in final): #add if letter is not already in
 						#final.append(str(letterName)[0:1])
-						final[str(letterName[0:1])] = maxVal
-					elif (final[str(letterName[0:1])] < maxVal): #add if current score is better than existing one
-						final[str(letterName[0:1])] = maxVal
+						final.setdefault(str(letterName[0:1]),[maxVal,maxLoc])
+						#final[str(letterName[0:1])] = maxVal
+					elif (distance(final[str(letterName[0:1])][1],maxLoc) > 10):
+						print (distance(final[str(letterName[0:1])][1],maxLoc))
+						final.setdefault(str(letterName[0:1]),[maxVal,maxLoc])
+					elif (final[str(letterName[0:1])][0] < maxVal): #add if current score is better than existing one
+						final[str(letterName[0:1])] = [maxVal,maxLoc]
+			iteration += 1
+	print ('expected', str(os.listdir(gameScreenshotPath)[screenshotPos]))
+	#print ('expecting', len(locations), 'letters')
+	print ('found', len(final), 'letters')
+	print ('Average ratio', (totalRatio / count))
+	return final
+
+def smartMatchLetters(gameScreenshotPath,screenshotPos, alphabetPath):
+	final = {}
+	locations = []
+	screenshot = cv2.imread(gameScreenshotPath + '/' + os.listdir(gameScreenshotPath)[screenshotPos], cv2.IMREAD_GRAYSCALE)
+	screenshotWidth = int(screenshot.shape[1]) #width of screenshot
+	print('ORIGINAL WIDTH', screenshotWidth)
+	'''
+	for scale in np.linspace(0.2,2,100)[::-1]:
+		print ('SCALE:',scale)
+		resized = imutils.resize(screenshot, width = int(screenshot.shape[1] * scale))
+		r = screenshot.shape[1] / float(resized.shape[1]) #ratio of old to new
+	'''
+	for letterName in os.listdir(alphabetPath):
+
+		#print('Looking for', str(letterName)[0:1])
+
+		colorImage = cv2.imread(alphabetPath + '/' + str(letterName))
+		letterImage = cv2.imread(alphabetPath + '/' + str(letterName), cv2.IMREAD_GRAYSCALE)
+		rows,cols = letterImage.shape
+		ratio = (cols/screenshotWidth)
+		print ('RATIO of LETTER to SCREENSHOT', ratio)
+
+		resized = imutils.resize(screenshot,width = 10 * cols)
+		tempRows,tempCols = resized.shape
+		print ('RESIZED WIDTH', tempCols)
+		print ('RATIO OF LETTER to RESIZED:', (tempCols/cols))
+		if (tempRows > rows and tempCols > cols):
+			
+			res = cv2.matchTemplate(resized, letterImage, cv2.TM_CCOEFF_NORMED)
+			(_,maxVal,_,maxLoc) = cv2.minMaxLoc(res)
+
+			xRange = range(maxLoc[0] - 10, maxLoc[1] + 10)
+			#print (maxVal)
+			threshold = 0.9
+			if (maxVal >= threshold): #only if decent match
+				print (str(letterName[0:1]), maxVal)
+				if (str(letterName[0:1]) not in final): #add if letter is not already in
+					#final.append(str(letterName)[0:1])
+					final[str(letterName[0:1])] = maxVal
+				elif (final[str(letterName[0:1])] < maxVal): #add if current score is better than existing one
+					final[str(letterName[0:1])] = maxVal
 
 	print ('expected', str(os.listdir(gameScreenshotPath)[screenshotPos]))
 	#print ('expecting', len(locations), 'letters')
 	print ('found', len(final), 'letters')
 	return final
-
 def topMatches(scoreList,expectedNum): #list of scores, expected number of letters
 	final = [expectedNum]
 	for score in scoreList:
