845bbb1cb81b6ab04c0029cd7793ca3efb1d83ca
Colin Chen <colin.a.chen@gmail.com>
2018-12-08 20:15:48 -0800

    better multiscale matching

--- processImage.py
+++ processImage.py
@@ -36,6 +36,16 @@ def display (title, img):
 #format a single image (go to black and white)
 def distance(point1, point2):
 	return math.sqrt(((point1[0] - point2[0]) ** 2) + ((point1[1] - point2[1]) ** 2) )
+def findInList(searchList,value):
+	index = -1
+	indicies = []
+	while(True):
+		try:
+			index = searchList.index(value, index+1)
+		except ValueError:
+			return indicies
+		indicies.append(index)
+
 def formatLetter(img): 
 	
 	
@@ -145,6 +155,91 @@ def matchLetters(gameScreenshotPath,screenshotPos, alphabetPath):
 	print ('Average ratio', (totalRatio / count))
 	return final
 
+
+
+
+
+def newMatchLetters(gameScreenshotPath,screenshotPos, alphabetPath):
+	final = []
+	locs = []
+	temp = []
+	temporaryApp = []
+	iteration = 0
+	screenshot = cv2.imread(gameScreenshotPath + '/' + os.listdir(gameScreenshotPath)[screenshotPos], cv2.IMREAD_GRAYSCALE)
+	for scale in np.linspace(0.2,2,100)[::-1]:
+		print ('SCALE:',scale)
+		resized = imutils.resize(screenshot, width = int(screenshot.shape[1] * scale))
+		r = screenshot.shape[1] / float(resized.shape[1]) #ratio of old to new
+
+		for letterName in os.listdir(alphabetPath): #for each letter
+			letter = str(letterName[0:1])
+			#print('Looking for', str(letterName)[0:1])
+
+			colorImage = cv2.imread(alphabetPath + '/' + str(letterName))
+			letterImage = cv2.imread(alphabetPath + '/' + str(letterName), cv2.IMREAD_GRAYSCALE)
+			rows,cols = letterImage.shape
+			tempRows,tempCols = resized.shape
+
+			if (tempRows > rows and tempCols > cols):
+				res = cv2.matchTemplate(resized, letterImage, cv2.TM_CCOEFF_NORMED)
+				
+				#(_,maxVal,_,maxLoc) = cv2.minMaxLoc(res)
+				threshold = 0.9
+				locations = np.where(res >= threshold)
+				for point in list(zip(*locations[::-1])): #for each matching point of a single letter, can be matches at diff locs
+					temp = [letter,point]	#temp holds the letter and the point
+
+					if (letter not in final):	#add letter if it is new 
+						final.append(letter)
+						locs.append(point)
+						print ('adding new letter', letter)
+					else:
+						print ('looking for old letter', letter)
+						letterIndex = findInList(final, letter)
+						print (letterIndex)
+						for index in letterIndex:
+							if (distance(point,locs[index]) > 75):
+								print (distance(point,locs[index]))
+								final.append(letter)
+								locs.append(point)					
+				'''
+						for letterPoint in final:
+							print (letterPoint)
+							if (str(letterName[0:1]) not in letterPoint[0]): #add if new letter		
+								final.append(temp)
+							elif (distance(letterPoint[1],point) > 50):
+								#print ('DISTANCE',distance(letterPoint[1],point))
+
+								final.append(temp)
+				'''	
+					
+				'''	
+				if (maxVal >= threshold): #only if decent match
+					letterToScreen = cols/(resized.shape[1])
+					totalRatio += letterToScreen
+					count += 1
+					print (str(letterName[0:1]), maxVal)
+					print ('RATIO', letterToScreen)
+					print ('MAXLOC', maxLoc)
+					temp = [str(letterName[0:1]), maxVal, maxLoc]
+					for letter in final:
+
+						if (str(letterName[0:1]) not in letter[0]): #add if new letter
+							
+							final.append(temp)
+							
+						elif (distance(letter[2],maxLoc) > 10):
+							print ('DISTANCE',distance(final[str(letterName[0:1])][1],maxLoc))
+							final.append(temp)
+						#elif (final[str(letterName[0:1])][0] < maxVal): #add if current score is better than existing one
+							#final[str(letterName[0:1])] = [maxVal,maxLoc]
+				'''
+	print ('expected', str(os.listdir(gameScreenshotPath)[screenshotPos]))
+	#print ('expecting', len(locations), 'letters')
+	print ('found', len(final), 'letters')
+	
+	return final
+
 def smartMatchLetters(gameScreenshotPath,screenshotPos, alphabetPath):
 	final = {}
 	locations = []
@@ -209,7 +304,7 @@ def main():
 	#print (matchLetters(pathScreeshot,pathForm))
 
 	formatAll(pathUnForm, pathForm)
-	letters = matchLetters(pathScreenshot,int(sys.argv[1]),pathForm)
+	letters = newMatchLetters(pathScreenshot,int(sys.argv[1]),pathForm)
 	print (letters)
 	print (wordMocha.solve(letters))
 	'''
